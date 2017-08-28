e102b681c3d96cf5abf51168c06bffe6b6d8c541
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2017-08-28 18:31:22 -0400

    update

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,64 @@
 class Elevator {
   constructor(){
     this.floor      = 0;
+
+    // current direction that the elevator is moving in
+    // ('up' or 'down');
+    this.direction = 'up';
+
+    // max value of 'this.floor'
     this.MAXFLOOR   = 10;
+
+    // an array of people in the elevator waiting to be dropped off;
+    this.passengers = [];
+
+    // an array of people that are witing to be picked up
+    this.waitingList = [];
+
+    // an array of numbers of the floors which the elevator must stop
+    // either to drop off or pick up
     this.requests   = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    // begin a set interval that calls "update()" every second
+    this.cow = setInterval(() => {
+      this.update()
+    }, 1000)
+  }
+
+  stop() {
+    // cancel the setInterval started by "start()"
+    clearInterval(this.cow);
+  }
+
+  update() {
+    // for now, will only call 'log()'
+    this.log();
+  }
+
+  log() { // displays the current direction and the current floor
+      //"Direction: up | Floor 5";
+      console.log("Direction is: " + this.direction + " | Floor: " + this.floor);
+   }
+
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
+
+  floorUp() {
+    if (this.floor => 10) {
+      console.log("can't go up");;
+    } else (this.floor < 10) {
+      this.floor++;
+    }
+  }
+
+  floorDown() {
+    if (this.floor >= 10) {
+      this.floor--;
+    }
+  }
   call() { }
-  log() { }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,21 @@
-const Elevator = require('./elevator.js');
+// import the elevator and the person file
+const Elevator  = require('./elevator.js');
+const Person    = require('./person.js');
+
+//create our elevator
+const myElevator = new Elevator();
+
+// start the elevator automatic movement
+myElevator.start();
+
+// For iteration 2; call "floorUp()" and "floorDown"
+
+// myElevator.floorUp();
+myElevator.floorUp();
+
+// myElevator.floorDown();
+
+// stop the elevator after 20 seconds.
+setTimeout(() => {
+  myElevator.stop();
+}, 20000);
